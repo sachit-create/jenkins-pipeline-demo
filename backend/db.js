6c1d7dffea1526db78e80d75a5edd146755a98ed
Ubuntu <ubuntu@ip-172-31-47-133.ap-south-1.compute.internal>
2026-02-20 14:42:18 +0000

    new changes

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config(); 
 
-const uri = process.env.MONGODB_URI
+const uri = "mongodb+srv://abhinavsharma:abhinav99@cluster0.bhisjbw.mongodb.net/blogify?retryWrites=true&w=majority"; 
 
 const client = new MongoClient(uri, {
   serverApi: {
